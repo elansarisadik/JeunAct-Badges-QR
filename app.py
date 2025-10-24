38553368637c2f452d815883a0212b7442c5c777
elansarisadik <elansarisadik@gmail.com>
2025-10-25 00:02:31 +0100

    Fix: Change default port from 5000 to 8000 for Koyeb deployment

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ if __name__ == '__main__':
         db.create_all()
     
     # Configuration pour la production
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     debug = os.environ.get('FLASK_ENV') != 'production'
     
     app.run(host='0.0.0.0', port=port, debug=debug)
